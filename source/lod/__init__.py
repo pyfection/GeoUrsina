4662190eaf7a883546c6c63b9f142703c9e2d1e4
pyfection <matthias.schreiber@zoho.com>
2023-01-04 17:28:03 -0300

    Add new ViewController to behave more like an entity

--- source/lod/__init__.py
+++ source/lod/__init__.py
@@ -1,13 +1,19 @@
 from typing import TYPE_CHECKING
 
-from ursina import EditorCamera
+from ursina import Vec3
+
+from viewcontroller import ViewController
 
 if TYPE_CHECKING:
     from map_view import MapView
 
 
-class LOD(EditorCamera):
+class LOD(ViewController):
     """Base class for Level Of Detail calculations."""
     def __init__(self, view: "MapView"):
         self.view = view
         super().__init__()
+        self.position = Vec3(0, 0, -5)
+        self.rotation_smoothing = 10
+        self.origin_y = -0.5
+        self.speed = 1
